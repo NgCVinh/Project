fe871b2e39763c8e9c5c0941782530aff03327c0
VinhCong <congvinh2701@gmail.com>
2024-04-13 21:08:08 +0700

    Hello

---
+++ HW1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.print("Họ và tên:Nguyễn Công Vinh\t");
+        System.out.print("MSV:23020445\t");
+        System.out.print("Lớp:K68-AI1\t");
+        System.out.print("Username:NgCVinh\t");
+        System.out.print("Email:congvinh2701@gmail.com\n");
+        for(int i=9;i>=1;i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
